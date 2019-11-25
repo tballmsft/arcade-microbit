c58c30bbb72a8e95468218b7dac9358adfe98ac5
Tom Ball <tballmsft@users.noreply.github.com>
2019-11-25 13:22:22 -0800

    some fixes

--- microbit.ts
+++ microbit.ts
@@ -1,4 +1,4 @@
-// Add your code here
+//%
 namespace microbit {
 
 let microbit = img`
@@ -81,6 +81,7 @@ for (let z = 0; z <= 4; z++) {
     }
 }
 
+//% block
 export function plot(x: number, y: number) {
     let index = 5 * x + y
     if (0 <= index && index < 25) {
@@ -89,14 +90,7 @@ export function plot(x: number, y: number) {
     }
 }
 
-export function toggle(x: number, y: number) {
-    let index = 5 * x + y
-    if (0 <= index && index < 25) {
-        flags[index] = !flags[index];
-        leds[index].setFlag(SpriteFlag.Invisible, flags[index])
-    }
-}
-
+//% block
 export function unplot(x: number, y: number) {
     let index = 5 * x + y
     if (0 <= index && index < 25) {
@@ -105,12 +99,105 @@ export function unplot(x: number, y: number) {
     }
 }
 
+//% block
 export function point(x: number, y: number) {
     let index = 5 * x + y;
     if (0 <= index && index < 25) {
-        return flags[index]
+        return !flags[index]
     }
     return false;
 }
+
+
+/**
+ * Toggles a particular pixel
+ * @param x pixel column
+ * @param y pixel row
+ */
+//% weight=77
+//% blockId=device_led_toggle block="toggle|x %x|y %y" icon="\uf204" blockGap=8
+//% x.min=0 x.max=4 y.min=0 y.max=4
+//% x.fieldOptions.precision=1 y.fieldOptions.precision=1
+export function toggle(x: number, y: number): void {
+    if (point(x, y)) {
+        unplot(x, y);
+    } else {
+        plot(x, y);
+    }
+}
+
+/**
+ * Turns all LEDS on
+ */
+//%
+export function plotAll(): void {
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 5; j++) {
+            plot(i, j);
+        }
+    }
+}
+
+/**
+ * Inverts the current LED display
+ */
+//%
+export function toggleAll(): void {
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 5; j++) {
+            toggle(i, j);
+        }
+    }
+}
+
+// what's the current high value
+let barGraphHigh = 0;
+// when was the current high value recorded
+let barGraphHighLast = 0;
+
+/**
+ * Displays a vertical bar graph based on the `value` and `high` value.
+ * If `high` is 0, the chart gets adjusted automatically.
+ * @param value current value to plot
+ * @param high maximum value. If 0, maximum value adjusted automatically, eg: 0
+ */
+//% help=led/plot-bar-graph weight=20
+//% blockId=device_plot_bar_graph block="plot bar graph of %value up to %high" icon="\uf080" blockExternalInputs=true
+//% parts="ledmatrix"
+export function plotBarGraph(value: number, high: number): void {
+    const now = 0;
+    console.logValue("", value);
+    value = Math.abs(value);
+
+    // auto-scale "high" is not provided
+    if (high > 0) {
+        barGraphHigh = high;
+    } else if (value > barGraphHigh || now - barGraphHighLast > 10000) {
+        barGraphHigh = value;
+        barGraphHighLast = now;
+    }
+
+    // normalize lack of data to 0..1
+    if (barGraphHigh < 16 * Number.EPSILON)
+        barGraphHigh = 1;
+
+    // normalize value to 0..1
+    const v = value / barGraphHigh;
+    const dv = 1 / 16;
+    let k = 0;
+    for (let y = 4; y >= 0; --y) {
+        for (let x = 0; x < 3; ++x) {
+            if (k > v) {
+                unplot(2 - x, y);
+                unplot(2 + x, y);
+            } else {
+                plot(2 - x, y);
+                plot(2 + x, y);
+            }
+            k += dv;
+        }
+    }
+}
+
 
 }
